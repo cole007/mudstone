4c529706cc6ede2f738ed40ebd838b3cd1fc37e1
Cole Henley <cole@tinyv.com>
2014-11-05 12:09:16 +0000

    Updated core framework for JS

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -1,3 +1,23 @@
-jQuery(function($){
+// declare our JS framework, HT @jackfranklin
+var doJqueryAction = {
+    // variables you want available on page load
+    getVars: function () {
+        'use strict';
+    	// eg this.accordion = $('.accordion');
+    },    
+    // functions you want to kick in on page load
+    init: function () {
+      'use strict';
+      this.getVars();
+    	// eg  this.doShowcase();
+    },
+    // your functions
+    doShowcase: function() {
+      // create local instance of this
+      var self = this;
+    }
+};
 
+jQuery(function($){
+  doJqueryAction.init();
 });
