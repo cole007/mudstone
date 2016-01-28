791daa2eef3c7f1ab35413ebe52f7ff8e63e9f2c
Dave Stockley <leon@ournameismud.co.uk>
2016-01-28 14:36:28 +0000

    png spriting fixed

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -10,11 +10,13 @@ var gulp            = require('gulp'),
     config          = require('../config').sprites,
     handleErrors    = require('../util/handleErrors'),
     browserSync     = require('browser-sync'),
+    buffer          = require('vinyl-buffer'),
+    merge           = require('merge-stream'),
     reload          = browserSync.reload;
 
 
-
 gulp.task('sprite', function () {
+  // Generate our spritesheet
     var spriteData = gulp.src(config.data)
     .pipe(spritesmith({
         imgName: config.imgName,
@@ -25,10 +27,39 @@ gulp.task('sprite', function () {
         }
     }))
     .on('error', handleErrors);
-    spriteData.img
-        .pipe(imagemin())
-        .pipe(gulp.dest(config.spriteDataImg));
-    spriteData.css
-        .pipe(gulp.dest(config.spriteDataCss))
-        .pipe(browserSync.reload({stream:true}));
+
+  // Pipe image stream through image optimizer and onto disk
+  var imgStream = spriteData.img
+    // DEV: We must buffer our stream into a Buffer for `imagemin`
+    .pipe(buffer())
+    .pipe(imagemin())
+    .pipe(gulp.dest(config.spriteDataImg));
+
+  // Pipe CSS stream through CSS optimizer and onto disk
+  var cssStream = spriteData.css
+    .pipe(gulp.dest(config.spriteDataCss));
+
+  // Return a merged stream to handle both `end` events
+  return merge(imgStream, cssStream);
 });
+
+
+
+// gulp.task('sprite', function () {
+//     var spriteData = gulp.src(config.data)
+//     .pipe(spritesmith({
+//         imgName: config.imgName,
+//         cssName: config.cssName,
+//         imgPath: config.imgPath,
+//         cssVarMap: function (sprite) {
+//             sprite.name = sprite.name;
+//         }
+//     }))
+//     .on('error', handleErrors);
+//     spriteData.img
+//         .pipe(imagemin())
+//         .pipe(gulp.dest(config.spriteDataImg));
+//     spriteData.css
+//         .pipe(gulp.dest(config.spriteDataCss))
+//         .pipe(browserSync.reload({stream:true}));
+// });
