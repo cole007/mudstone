80aff0fcce3722e42c96fae03424abf4d4d67ac9
Dave Stockley <leon@ournameismud.co.uk>
2016-01-31 18:32:44 +0000

    scripts fix

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -6,25 +6,25 @@
 
 
 var gulp         = require('gulp'),
-	browserSync  	= require('browser-sync'),
-	uglify       	= require('gulp-uglify'),
-	concat       	= require('gulp-concat'),
-	babel 				= require('gulp-babel'),
-	sourcemaps   	= require('gulp-sourcemaps'),
-	handleErrors 	= require('../util/handleErrors'),
-	config       	= require('../config').scripts,
-	gulpif		 		= require('gulp-if'),
-	env 		 			= require('../config').env;
+	browserSync  = require('browser-sync'),
+	uglify       = require('gulp-uglify'),
+	concat       = require('gulp-concat'),
+	babel 		 = require('gulp-babel'),
+	sourcemaps   = require('gulp-sourcemaps'),
+	handleErrors = require('../util/handleErrors'),
+	config       = require('../config').scripts,
+	gulpif		 = require('gulp-if'),
+	env 		 = require('../config').env;
 
 
 gulp.task('scripts', function() {
   return gulp.src(config.src)
     .pipe(gulpif(env == 'dev', sourcemaps.init()))
     .pipe(gulpif(env == 'live', uglify()))
+	.pipe(babel({
+		presets: ['es2015']
+	}))
     .on('error', handleErrors)
-		.pipe(babel({
-			presets: ['es2015']
-		}))
     .pipe(concat(config.output))
     .pipe(gulpif(env == 'dev', sourcemaps.write('./')))
     .pipe(gulp.dest(config.dest))
