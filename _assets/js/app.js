a64ad43517cad48f26267ea1d5f7ba48b7297e59
Dave Stockley <leon@ournameismud.co.uk>
2016-04-14 14:51:02 +0100

    script fix

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -6,8 +6,7 @@ import validation from './behaviours/validation';
 import carousel from './behaviours/carousel';
 import { megaNav } from  './behaviours/megaNav';
 import prefix from './helpers/prefix';
-import lazyLoad from './behaviours/lazyLoad';
-import loadVideo from './behaviours/loadVideo';
+
 
 WebFont.load({
 	typekit: { id: 'vcl8lns' }
@@ -20,8 +19,6 @@ mud.Behaviours.accordion = accordion;
 mud.Behaviours.validation = validation;
 mud.Behaviours.megaNav = megaNav;
 mud.Behaviours.carousel = carousel;
-mud.LoadWindow.lazyLoad = lazyLoad;
-mud.LoadWindow.loadVideo = loadVideo;
 
 window.onload = function(){
     mud.onWindowLoad();
