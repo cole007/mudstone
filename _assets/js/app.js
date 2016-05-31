81a41b6c44d9b7e9e311eb0616e160a723ac9ac8
Dave Stockley <leon@ournameismud.co.uk>
2016-05-31 23:34:20 +0100

    removed example js, vars block added to jade wrapper and index

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,20 +1,8 @@
 import WebFont from 'webfontloader';
 import mud from './dependencies/load-behaviour';
-import svg4everybody from 'svg4everybody';
+// import svg4everybody from 'svg4everybody';
 // object-fit polyfill https://github.com/bfred-it/object-fit-images
 // import objectFitImages from 'object-fit-images';
-
-import { megaNav } from  './behaviours/megaNav';
-import canvasNavigation from './behaviours/canvasNavigation';
-
-import accordion from './behaviours/accordion';
-import carousel from './behaviours/carousel';
-import slide from './behaviours/slide';
-import video from './behaviours/video';
-import form from './behaviours/form';
-import map from './behaviours/map';
-
-
 
 // https://github.com/typekit/webfontloader
 WebFont.load({
@@ -26,26 +14,13 @@ WebFont.load({
 	// }
 });
 
-
-// assign the data-behaviour functions
-mud.Behaviours.canvasNavigation = canvasNavigation;
-mud.Behaviours.megaNav = megaNav;
-
-
-mud.Behaviours.accordion = accordion;
-mud.Behaviours.carousel = carousel;
-mud.Behaviours.slide = slide;
-mud.Behaviours.video = video;
-mud.Behaviours.form = form;
-mud.Behaviours.map = map;
-
 window.onload = function(){
     mud.onWindowLoad();
 };
 
 $(function() {
 	mud.loadBehaviour();
-	svg4everybody();
+	// svg4everybody();
 	// var someImages = document.querySelectorAll('img.u-fit');
 	// objectFitImages(someImages);
 });
