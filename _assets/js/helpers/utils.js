19103846d54615eb34e4fbdd01113dd18adbc61c
Dave Stockley <leon@ournameismud.co.uk>
2016-05-31 23:36:30 +0100

    removed js examples

--- _assets/js/helpers/utils.js
+++ _assets/js/helpers/utils.js
@@ -19,58 +19,7 @@ const utils = {
 		}
 	},
 
-	firstSegment() {
-		return window.location.pathname.replace(/^\/([^\/]*).*$/, '$1').split('.')[0];
-	},
-
-	segmentCount() {
-		return window.location.pathname.split('/').length - 1;
-	},
-
-	whatUrl() {
-		var firstSegment = utils.firstSegment();
-		return firstSegment != '' ? firstSegment : 'index';
-	},
-
-	// returns a function containing the data from an ajax request
-	// only makes ajax request if no content is cache
-	json: function(fn) {
-		if(content === undefined) {
-			$.getJSON( '/content.json').done(function( data ) {
-				fn(data);
-				content = data;
-			});
-		} else {
-			fn(content);
-		}
-	},
-
-	segment1s(string) {
-		var urls = Object.keys(this.json);
-		var result = false;
-		urls.forEach((element) => {
-			if(element === string) {
-				result = true;
-			}
-		});
-
-		return result;
-	},
-
-	getHourAngle(currentTime) {
-		var hour = currentTime.getHours(),
-			mins = currentTime.getMinutes() / 60,
-			hour = hour > 12 ? (hour - 12 + mins) * 30 : (hour + mins) * 30;
-			return hour;
-	},
-
-	getMinuteAngle(currentTime) {
-		return currentTime.getMinutes() * 6;
-	},
-
-	rando(min, max) {
-	  return Math.floor(Math.random() * (max - min)) + min;
-	},
+	
 
 	ajax: function(url) {
 		var data = {};
