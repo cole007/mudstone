3dce50e4a59365e8b0e9cd198b85b6bd8dc5f325
Dave Stockley <leon@ournameismud.co.uk>
2016-07-12 23:32:17 +0100

    removed exapnder test

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -5,42 +5,6 @@ import mud from './loader/behaviour';
 // object-fit polyfill https://github.com/bfred-it/object-fit-images
 // import objectFitImages from 'object-fit-images';
 
-import $ from 'jquery';
-import Expand from './helpers/expand';
-
-
-mud.Behaviours.accordion = function(container) {
-
-	var accordion = new Expand({
-		wrapper: $(container),
-		button: '.button',
-		closeOthers: false,
-		activeClass: '.is-active',
-		duration: 1000,
-		openStart: function(el, target) {
-			console.log('openStart callback');
-			console.log('this', this, 'el', el, 'target', target);
-		},
-		openComplete: function(el, target) {
-			console.log('openComplete callback');
-			console.log('this', this, 'el', el, 'target', target);
-		},
-		closeStart: function(el, target) {
-			console.log('closeStart callback')
-			console.log('this', this, 'el', el, 'target', target);
-		},
-		closeComplete: function(el, target) {
-			console.log('closeComplete callback')
-			console.log('this', this, 'el', el, 'target', target);
-		},
-		onTick: function(v, el, target) {
-			console.log('this', this, 'v', v, 'el', el, 'target', target);
-		
-		}
-	});
-
-}
-
 // https://github.com/typekit/webfontloader
 WebFont.load({
 	// google: { families: ['Droid Sans', 'Droid Serif'] },
