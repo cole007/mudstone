29bb8724ea2dd4fbf0ae5de2af55f94920bd8196
Jay Collett <jayboxdesigns@gmail.com>
2017-01-27 15:26:27 +0000

    turning on mobile menu js out the box

--- _assets/js/behaviours/index.js
+++ _assets/js/behaviours/index.js
@@ -1,10 +1,9 @@
 // export { video } from './video'
 // export { map } from './map'
-// export { mobileMenu } from './mobileMenu'
 // export { carousel } from './carousel'
 // export { accordion } from './accordion'
 // export { formValidation } from './formValidation'
 // export { inview } from './inview'
 
 
-export { test2, test } from './test'
+export { mobileMenu } from './mobileMenu'
