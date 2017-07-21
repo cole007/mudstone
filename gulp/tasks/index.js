ffa82ea338e9c4c450335585f26b2d5b2a960fde
David Maxwell-Wood <dhmax88@gmail.com>
2017-07-21 09:41:08 +0100

    added twig task to gulp

--- gulp/tasks/index.js
+++ gulp/tasks/index.js
@@ -3,6 +3,7 @@ export { html } from './html'
 export { images } from './images'
 export { svgs } from './svgs'
 export { symbols } from './symbols'
+export { twig } from './twig'
 export {
 	fontsTask,
 	faviconsTask,
