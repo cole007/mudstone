7b5165e870dc2eb271871fe55e0c4c6774b7231c
Dave Stockley <leon@ournameismud.co.uk>
2017-08-01 16:44:19 +0100

    comments added to js form validation helper

--- src/js/ui/validation.js
+++ src/js/ui/validation.js
@@ -7,6 +7,19 @@ import { mergeOptions } from '@/utils/helpers'
 import { DomClass, DomClosest } from '@/utils/dom'
 
 
+/**
+ * 
+ * @class Validation
+ * @extends  Concert
+ * @param  {HTMLElement} el : the form to validate
+ * @param  {Object} options : validation options
+ * 									init: Boolean // enable the validation
+ * 									group: Boolean/String // field grouping element, if boolean input.parentNode is used
+ * 									ajax: Boolean // to post via ajax
+ * 									emptyFieldsOnComplete: Boolean // reset fields on submit
+ * 									constraints: Object // the validation rules, see validatejs.org for more information
+ */
+
 export default class Validation extends Concert {
 
 	defaults = {
@@ -37,6 +50,13 @@ export default class Validation extends Concert {
 		}
 	}
 
+	/**
+	 * 
+	 * @function constructor
+	 * @param  {HTMLElement} el : the form to validate
+	 * @param  {Object} options : validation options
+	 * @return Validation
+	 */
 	constructor(el, options = {}) {
 		super()
 		this.$form = el
@@ -55,22 +75,64 @@ export default class Validation extends Concert {
 		this.options.init && this.initalize()
 	}
 
-	setForm = (el) => this.$form = el
+	/**
+	 * Update the form to validate, useful when forms are within modals
+	 * 
+	 * @function setForm
+	 * @param  {HTMLElement} el : the form to validate
+	 * @return Validation
+	 */
+	setForm = (el) => {
+		this.$form = el
 
+		return this
+	}
+
+	/**
+	 * Bind events
+	 * 
+	 * @function addEvents
+	 * @return Validation
+	 */
 	addEvents = () => {
 		this.delegate.on('change', '[data-validate]', this.onChange)
 		this.$form.addEventListener('submit', this.onSubmit)
+
+		return this
 	}
 
+	/**
+	 * Unbind events
+	 * 
+	 * @function removeEvents
+	 * @return Validation
+	 */
 	removeEvents = () => {
 		this.delegate.off('change', '[data-validate]', this.onChange)
 		this.$form.removeEventListener('submit', this.onSubmit)
+		
+		return this
 	}
 
+	/**
+	 * The 'change' event handler
+	 * 
+	 * @function onChange
+	 * @param  {Object} event : event object
+	 * @param  {HTMLElement} element : the input that's changed
+	 * @return Void
+	 */
 	onChange = (event, element) => {
 		this.showError(element)
 	}
 
+	/**
+	 * Checks the current input value against the constraints
+	 * 
+	 * @function showError
+	 * @param  {HTMLElement} element : the input to validate
+	 * @return Void
+	 */
 	showError = (element) => {
 		const name = element.getAttribute('name')
 		const instance = this.collection.find(item => item.name === name)
@@ -89,6 +151,13 @@ export default class Validation extends Concert {
 		}
 	}
 
+	/**
+	 * The form 'submit' handler
+	 * 
+	 * @function showError
+	 * @param  {Object} event : event object
+	 * @return Void
+	 */
 	onSubmit = (event) => {
 		const { constraints, ajax } = this.options
 		const errors = validate(this.$form, constraints, {fullMessages: false})
@@ -104,6 +173,12 @@ export default class Validation extends Concert {
 		!errors && this.postForm()
 	}
 
+	/**
+	 * Post the form via ajax
+	 * 
+	 * @function postForm
+	 * @return Void
+	 */
 	postForm = () => {
 		const data = new FormData(this.$form)
 		const { emptyFieldsOnComplete } = this.options
@@ -123,6 +198,14 @@ export default class Validation extends Concert {
 
 	}
 
+	/**
+	 * Add error message
+	 * 
+	 * @function renderMessage
+	 * @param  {HTMLElement, HTMLElement, HTMLElement} $message, $parent, $node : deconstructed argument containing all of the relevant html elements
+	 * @param  {Array} message : an array of messages, only the first[0] message is displayed
+	 * @return Void
+	 */
 	renderMessage = ({$message, $parent, $node}, message) => {
 		if(message[0] === $message.textContent) return
 		$message.textContent = ''
@@ -131,16 +214,38 @@ export default class Validation extends Concert {
 		DomClass($node).add('is-error')
 	}
 
+	/**
+	 * Remove the error message
+	 * 
+	 * @function renderMessage
+	 * @param  {HTMLElement, HTMLElement, HTMLElement} $message, $parent, $node : deconstructed argument containing all of the relevant html elements
+	 * @return Void
+	 */
 	removeError = ({$message, $parent, $node}) => {
 		$message.textContent = ''
 		DomClass($parent).remove('is-error')
 		DomClass($node).remove('is-error')
 	}
 
+	/**
+	 * Set where to delegate the behaviours to, useful when forms are within modals
+	 * 
+	 * @function setDelegate
+	 * @param  {HTMLElement} el, html element to delegate events
+	 * @return Validation
+	 */
 	setDelegate = (el = this.$form) => {
 		this.delegate = new Delegate(el)
+		
+		return this
 	}
 
+	/**
+	 * Setup the dom, add error nodes to markup
+	 * 
+	 * @function setupDom
+	 * @return Validation
+	 */
 	setupDom = () => {
 		this.$form.setAttribute('novalidate', true)
 		
@@ -160,15 +265,29 @@ export default class Validation extends Concert {
 			})
 			this.collection = [...this.collection, ...nodes]
 		}
+		
+		return this
 	}
 
+	/**
+	 * Enable all the things
+	 * 
+	 * @function initalize
+	 * @return Void
+	 */
 	initalize = () => {
 		this.collection = []
 		this.setDelegate()
-		this.setupDom()
-		this.addEvents()
+				.setupDom()
+				.addEvents()
 	}
 
+	/**
+	 * Destroy all the things
+	 * 
+	 * @function destroy
+	 * @return Void
+	 */
 	destroy = () => {
 		this.collection = []
 		this.removeEvents()
