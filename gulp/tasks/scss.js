868c9dd68029861367b299b24b29fba4151dab21
magicspon <leon@ournameismud.co.uk>
2017-10-17 02:05:07 +0100

    updated to mud-fractal scss setup

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -16,60 +16,88 @@ import rucksack from 'rucksack-css'
 import browserSync from 'browser-sync'
 import { handleErrors } from '../libs/utils'
 import path from 'path'
+import sassGlob from 'gulp-sass-glob'
+import sassVariables from 'gulp-sass-variables'
 
 export function scss() {
-
-
 	const paths = {
-		src: path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.scss.src, '**/**/*.scss'),
+		src: path.resolve(
+			process.env.PWD,
+			PATH_CONFIG.src,
+			PATH_CONFIG.scss.src,
+			'**/**/*.scss'
+		),
 		dest: path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.scss.dest)
 	}
 
-
-	return gulp.src(paths.src)
-		.pipe(styleLint({
-			debug: true,
-			failAfterError: false,
-			syntax: 'scss',
-			reporters: [{
-				formatter: 'string',
-				console: true
-			}]
-		}))
+	return gulp
+		.src(paths.src)
+		.pipe(
+			styleLint({
+				debug: true,
+				failAfterError: false,
+				syntax: 'scss',
+				reporters: [
+					{
+						formatter: 'string',
+						console: true
+					}
+				]
+			})
+		)
 		.on('error', handleErrors)
 		.pipe(gulpif(!global.production, sourcemaps.init()))
-		.pipe(sass({
-			...TASK_CONFIG.scss.options,
-			includePaths: [
-				path.resolve(process.env.PWD, 'node_modules/normalize-scss/sass'),
-				path.resolve(process.env.PWD, 'node_modules/normalize-scss/sass'),
-				path.resolve(process.env.PWD, 'node_modules/susy/sass')
-			]
-		}))
+		.pipe(sassGlob())
+		.pipe(
+			sassVariables({
+				$env: global.production ? 'production' : 'development'
+			})
+		)
+		.pipe(
+			sass({
+				...TASK_CONFIG.scss.options,
+				includePaths: [
+					path.resolve(process.env.PWD, 'node_modules/normalize-scss/sass'),
+					path.resolve(process.env.PWD, 'node_modules/normalize-scss/sass'),
+					path.resolve(process.env.PWD, 'node_modules/susy/sass')
+				]
+			})
+		)
 		.on('error', handleErrors)
-		.pipe(gulpif(!global.production, sourcemaps.init({
-			loadMaps: true
-		})))
-		.pipe(postcss([
-			rucksack(),
-			objectFitImages(),
-			animateCss(),
-			aspectRatio(),
-			postcssTriangle(),
-			writeSVG({
-				encoding: 'base64'
-			}),
-			autoprefixer(TASK_CONFIG.scss.autoprefixer)
-		]))
+		.pipe(
+			gulpif(
+				!global.production,
+				sourcemaps.init({
+					loadMaps: true
+				})
+			)
+		)
+		.pipe(
+			postcss([
+				rucksack(),
+				objectFitImages(),
+				animateCss(),
+				aspectRatio(),
+				postcssTriangle(),
+				writeSVG({
+					encoding: 'base64'
+				}),
+				autoprefixer(TASK_CONFIG.scss.autoprefixer)
+			])
+		)
 		.on('error', handleErrors)
 		.pipe(gulpif(global.production, cssnano(TASK_CONFIG.scss.cssnanoOptions)))
 		.pipe(gulpif(!global.production, sourcemaps.write()))
-		.pipe(gulpif(global.production, rename({
-			suffix: `.${TASK_CONFIG.stamp}`
-		})))
+		.pipe(
+			gulpif(
+				global.production,
+				rename({
+					suffix: `.${TASK_CONFIG.stamp}`
+				})
+			)
+		)
 		.pipe(gulp.dest(paths.dest))
 		.pipe(browserSync.stream())
 }
-
 
 gulp.task('scss', scss)
